eef3b7a7766409d4e0755c6605bf83873de28a10
Gabriel Ferreiro <gbril9119@gmail.com>
2016-08-25 14:42:41 +0200

    Added runtime in detail view

---
+++ Tracky/UI/Converters/RuntimeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Tracky.UI.Converters
+{
+    public class RuntimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var runtime = (int?) value;
+            return $"{runtime ?? 0} minutes";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var runtime = value.ToString();
+            return int.Parse(runtime);
+        }
+    }
+}
