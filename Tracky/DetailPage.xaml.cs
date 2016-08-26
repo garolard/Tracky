877314f752d343be42f7a58f0421d27fbb9cc0e4
Gabriel Ferreiro <gbril9119@gmail.com>
2016-08-26 10:58:46 +0200

    Added transparent TitleBar in detail view

--- Tracky/DetailPage.xaml.cs
+++ Tracky/DetailPage.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
@@ -19,15 +23,55 @@ namespace Tracky
         public DetailPage()
         {
             this.InitializeComponent();
+            Window.Current.Activated += CurrentOnActivated;
         }
         
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBarOnLayoutMetricsChanged;
+            TitleBar.Height = coreTitleBar.Height;
+            Window.Current.SetTitleBar(MainTitleBar);
+
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.ApplicationView"))
+            {
+                var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+                if (titleBar != null)
+                {
+                    titleBar.ButtonBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+                }
+            }
+
             var show = (TraktShow)e.Parameter;
             var ctx = this.DataContext as DetailViewModel;
             await ctx.OnNavigatedTo(show);
+        }
+
+        private void CurrentOnActivated(object sender, WindowActivatedEventArgs windowActivatedEventArgs)
+        {
+            if (windowActivatedEventArgs.WindowActivationState != CoreWindowActivationState.Deactivated)
+            {
+                MainTitleBar.Opacity = 1;
+            }
+            else
+            {
+                MainTitleBar.Opacity = 0.5;
+            }
+        }
+
+        private void CoreTitleBarOnLayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            TitleBar.Height = sender.Height;
+            RightMask.Width = sender.SystemOverlayRightInset;
+        }
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
         }
     }
 }
